f4e898a5e78458ac11b3c7d4c76095e3cdad6080
Yasin Çoban <yasincoban1998@outlook.com>
2022-10-09 01:14:07 +0300

    Product manager updated

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,41 +1,51 @@
 ﻿using Business.Abstract;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Business.Concrete
 {
     public class ProductManager : IProductService
     {
-
-
-        public void Add(Product prd)
+        private IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
         {
+            _productDal = productDal;
+        }
+
+        public void Add(Product product)
+        {
+            //BUSINESS CODE
+            _productDal.Add(product);
 
         }
 
         public void Delete(Product product)
         {
-            throw new System.NotImplementedException();
+            _productDal.Delete(product);
+
         }
 
         public Product GetById(int productId)
         {
-            throw new System.NotImplementedException();
+            return _productDal.Get(x => x.ProductId == productId);
         }
 
         public List<Product> GetList()
         {
-            throw new System.NotImplementedException();
+            return _productDal.GetList().ToList();
         }
 
         public List<Product> GetListByCategory(int categoryId)
         {
-            throw new System.NotImplementedException();
+            return _productDal.GetList(x => x.CategoryId == categoryId).ToList();
+
         }
 
         public void Update(Product product)
         {
-            throw new System.NotImplementedException();
+            _productDal.Update(product);
         }
     }
 }
