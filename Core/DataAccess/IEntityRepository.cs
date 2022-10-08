e86fa6b2b0f69e40d2b94748d27145bb55e4854a
Yasin Çoban <yasincoban1998@outlook.com>
2022-10-09 00:05:59 +0300

    entities and repository added

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<T> where T : class,IEntity
+    {
+        T Get(Expression<Func<T, bool>> filter);
+        IList<T> GetList(Expression<Func<T,bool>> filter =null);
+
+        void Add(T entity);
+
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework.Context
+{
+    public class AppDbContext :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLOCALDB;Database=Nortwind;Trusted_Connection=true;");
+        }
+
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
