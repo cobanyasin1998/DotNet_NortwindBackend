b02e45ee4334f11a5345435331b61868f7c74bd1
Yasin Çoban <yasincoban1998@outlook.com>
2022-10-09 01:08:10 +0300

    business layer service-manager added

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,41 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+
+
+        public void Add(Product prd)
+        {
+
+        }
+
+        public void Delete(Product product)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Product GetById(int productId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Product> GetList()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<Product> GetListByCategory(int categoryId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
