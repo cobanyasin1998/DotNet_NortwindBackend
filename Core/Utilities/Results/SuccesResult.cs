027dccd977560cd16e139a94bb9959ad9e96e567
Yasin Çoban <yasincoban1998@outlook.com>
2022-10-09 01:39:05 +0300

    result utililies

---
+++ Core/Utilities/Results/SuccesResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Utilities.Results
+{
+    public class SuccesResult:Result
+    {
+        public SuccesResult(string message) : base(true, message)
+        {
+           
+        }
+        public SuccesResult() : base(true)
+        {
+        
+        }
+    }
+}
